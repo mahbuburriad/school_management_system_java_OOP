dd22f34c11f384be38d22da8e4ce1e7fdcd36a17
Mahbubur Riad <mahbubur.riad@gmail.com>
2020-04-20 06:31:48 +0600

    students all implemented by specification
    constuctor, getter and setter, update fees

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -9,4 +9,57 @@ package school.management.system;
 
 
 public class Student {
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+
+    /**
+     * build constructor for student
+     * To create a new student by initializing
+     * Fees for every student is $30,000
+     * Fees paid initially is 0.
+     * @param id id for the student: unique
+     * @param name name of the student
+     * @param grade grade of the student
+     */
+    public Student(int id, String name, int grade) {
+        this.feesPaid = 0;
+        this.feesTotal = 30000;
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    //Not going to alter student's name, student's id.
+
+    /**
+     * new grade of the student
+     * @param grade
+     */
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    /**
+     * keep adding the fees to feesPaid Field
+     * Add the fees to the fees paid.
+     * The school is going to receive the funds.
+     * @param fees
+     */
+    public void updateFeesPaid(int fees){
+        //continuously update the fees
+        this.feesPaid+=fees;
+    }
+
+
+
+
+
 }
