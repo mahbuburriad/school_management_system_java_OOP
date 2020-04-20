bfb127efb743f32eb014fec8486af3f9c7147bc6
Mahbubur Riad <mahbubur.riad@gmail.com>
2020-04-20 16:04:53 +0600

    create teacher class with all dependencies

---
+++ src/school/management/system/Teacher.java
@@ -0,0 +1,67 @@
+package school.management.system;
+
+/**
+ * Create by Mahbubur Riad
+ * This class is responsible for keeping the track
+ * of teacher's name, id, salary.
+ */
+
+public class Teacher {
+    private int id;
+    private String name;
+    private int salary;
+
+    /**
+     * Creates a new Teacher object
+     * @param id id for the teacher
+     * @param name name of the teacher
+     * @param salary salary of the teacher
+     */
+
+    public Teacher(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    /**
+     * @return the id of the teacher
+     */
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return name of the teacher
+     */
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return salary of the teacher
+     */
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    
+
+}
