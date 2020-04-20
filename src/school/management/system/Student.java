4e49baa8ea558fa7c44870b4ef6f27695de59a7c
Mahbubur Riad <mahbubur.riad@gmail.com>
2020-04-20 06:09:30 +0600

    student class introduction added

---
+++ src/school/management/system/Student.java
@@ -0,0 +1,12 @@
+package school.management.system;
+
+/*
+* Created by Mahbubur Riad (https://github.com/mahbuburriad/)
+* This class is responsible for keeping the
+* track of students fees, name, grade & fees
+* paid.
+*/
+
+
+public class Student {
+}
